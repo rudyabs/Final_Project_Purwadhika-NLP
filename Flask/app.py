34f9a5fe0ffdc1cc95d57ccc9691ee24426f2827
rudyabs <rudy.abs@live.com>
2019-07-20 17:03:00 +0700

    Flask Starter Template

---
+++ Flask/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Rudy',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'July 20, 2019'
+    },
+    {
+        'author':'Aunalla',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'July 21, 2019'
+    }
+]
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+   return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+   return render_template('about.html', title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
